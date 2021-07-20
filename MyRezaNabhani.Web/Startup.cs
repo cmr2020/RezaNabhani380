7f891580d370314718b31296fb01fcd235dc5f88
rezanabhani387 <rezanabhani387@gmail.com>
2021-07-20 20:11:54 +0430

    Template transfer

--- MyRezaNabhani.Web/Startup.cs
+++ MyRezaNabhani.Web/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -51,6 +52,9 @@ namespace MyRezaNabhani.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
