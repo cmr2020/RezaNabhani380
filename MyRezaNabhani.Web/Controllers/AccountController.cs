f7b9485798562c9daaf2177dec7d02c9fdef8f33
rezanabhani387 <rezanabhani387@gmail.com>
2021-07-27 21:53:55 +0430

    Create Login

--- MyRezaNabhani.Web/Controllers/AccountController.cs
+++ MyRezaNabhani.Web/Controllers/AccountController.cs
@@ -68,5 +68,53 @@ namespace MyRezaNabhani.Web.Controllers
             return View("SuccessRegister", user);
         }
 
+
+        #region Login
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var user = _userRepository.LoginUser(login);
+            if (user != null)
+            {
+                if (user.IsActive)
+                {
+                    //ToDO Login User
+                    ViewBag.IsSuccess = true;
+                    return View();
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "حساب کاربری شما فعال نمی باشد");
+                }
+            }
+            ModelState.AddModelError("Email", "کاربری با مشخصات وارد شده یافت نشد");
+            return View(login);
+        }
+
+        #endregion
+
+        #region Active Account
+
+        public IActionResult ActiveAccount(string id)
+        {
+            ViewBag.IsActive = _userRepository.ActiveAccount(id);
+            return View();
+        }
+
+        #endregion
+
+
     }
 }
